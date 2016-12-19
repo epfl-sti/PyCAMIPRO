19fecfefa06ee7d925e8d0ec88210c7d298d4516
Nicolas Reymond <nreymond@shuttle.epfl.ch>
2016-12-19 15:42:48 +0100

    add + commit readusbcamipro.py rm readusbcamipro_baltec.py and readusbcamipro_baltec.py

---
+++ readusbcamipro.py
@@ -0,0 +1,87 @@
+import usb.core
+import sys
+
+
+def read_usb_camipro_baltec():
+    # Baltech:
+    idVendor  = 0x13ad
+    idProduct = 0x9cab
+    interface = 0
+    timeout_secs = 5
+
+    dev = usb.core.find(idVendor=idVendor, idProduct=idProduct)
+    if (dev is None):
+        print ("Aucun lecteur !!!")
+        sys.exit()
+    if dev.is_kernel_driver_active(interface) is True:
+        #print "but we need to detach kernel driver"
+        dev.detach_kernel_driver(interface)
+
+    dev.set_configuration()
+    cfg = dev.get_active_configuration()
+    intf = list(cfg)[0]
+    ep = usb.util.find_descriptor(
+                intf,
+                # match the first IN endpoint
+                custom_match = \
+                lambda e: \
+                    usb.util.endpoint_direction(e.bEndpointAddress) == \
+                    usb.util.ENDPOINT_IN
+            )
+    assert ep is not None
+
+    while True:
+        try:
+                    data = ep.read(ep.wMaxPacketSize * 4, timeout_secs * 1000)
+                    o = []
+                    i = "".join([ (chr(d) if d >= 47 else "") for d in data])
+                    with open('pyusb', 'a') as file:
+                        file.write(i + '\n')
+                        file.close()
+                    o.append(i)
+                    print o[:]
+
+        except usb.core.USBError as e:
+            print e
+            break
+
+def read_usb_camipro_elatec():
+    idVendor = 0x09d8
+    idProduct = 0x0406
+    interface = 0
+    timeout_secs = 5
+
+    dev = usb.core.find(idVendor=idVendor, idProduct=idProduct)
+    if dev.is_kernel_driver_active(interface) is True:
+        #print "but we need to detach kernel driver"
+        dev.detach_kernel_driver(interface)
+
+    dev.set_configuration()
+    cfg = dev.get_active_configuration()
+    intf = list(cfg)[0]
+    ep = usb.util.find_descriptor(
+                intf,
+                # match the first IN endpoint
+                custom_match = \
+                lambda e: \
+                    usb.util.endpoint_direction(e.bEndpointAddress) == \
+                    usb.util.ENDPOINT_IN
+            )
+    assert ep is not None
+    while True:
+        try:
+                        data = ep.read(ep.wMaxPacketSize, timeout_secs * 1000)
+                        o = []
+                        i = "".join([ (chr(d) if d >= 32 else '') for d in data])
+                        o.append(i)
+                        file = open("pyusb","w")
+                        file.write(i)
+                        file.close()
+                        print o[0]
+                        #   print o[1]
+
+        except usb.core.USBError as e:
+            print e
+            break
+
+read_usb_camipro_baltec()
